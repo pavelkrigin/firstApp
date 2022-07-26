1b24e751acffd71df92a0400f2a85749b39077be
Pavel Krigin <pavel.krigin.85@gmail.com>
2022-07-26 18:38:43 +0300

    Initial Commit

---
+++ firstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstApp
+//
+//  Created by Pavel Krigin on 26.7.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
